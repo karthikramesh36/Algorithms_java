4695dee80ec3199858c0c51acb8291122fb184b9
karthik ramesh <karthik.ramesh@ced.com>
2018-03-23 16:11:41 -0500

    add questions

--- src/java_algos/MergeIntervals.java
+++ src/java_algos/MergeIntervals.java
@@ -8,7 +8,11 @@ import java.util.List;
 
 import org.junit.Test;
 
-
+//Given a collection of intervals, merge all overlapping intervals.
+//
+//For example,
+//Given [1,3],[2,6],[8,10],[15,18],
+//return [1,6],[8,10],[15,18].
 public class MergeIntervals {
 	
 	  public class Interval {
--- src/java_algos/ZigZagString.java
+++ src/java_algos/ZigZagString.java
@@ -5,10 +5,20 @@ import java.util.List;
 
 import org.junit.Test;
 
+//The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+//
+//P   A   H   N
+//A P L S I I G
+//Y   I   R
+//And then read line by line: "PAHNAPLSIIGYIR"
+//Write the code that will take a string and make this conversion given a number of rows:
+//
+//string convert(string text, int nRows);
+//convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
 
 public class ZigZagString {
 
-	// given a string , once written in zigzag format , print it out again
+
 	public String Convert(String s , int numberOfRows) {
 		
 		if(numberOfRows == 1 || s.length() == 1) {
