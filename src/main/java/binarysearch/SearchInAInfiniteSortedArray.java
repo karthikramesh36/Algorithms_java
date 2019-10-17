45d13db3254a3d288c0bfa29288d7ab5579ac41d
karthik <karthik@localhost>
2019-10-16 23:25:16 -0500

    add logic for infinte search

---
+++ src/main/java/binarysearch/SearchInAInfiniteSortedArray.java
@@ -0,0 +1,26 @@
+package binarysearch;
+
+public class SearchInAInfiniteSortedArray {
+	
+	public int Search(int[] arr, int target) {
+		
+		if (arr.length ==0)
+			return -1;
+		
+		int low =0, high = 2;
+		
+		while(arr[high] < target) {
+			low = high;
+			if(target < arr[high])
+				break;
+			
+			if( 2* high > arr.length-1)
+				high = arr.length-1;
+			else
+				high = 2 * high;
+			
+				
+		}
+	}
+
+}
